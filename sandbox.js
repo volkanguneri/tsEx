783ac35db939a042fe4977e52ef896cb10163a0b
Vocci <vocci[Chotmail.com>
2024-03-25 09:35:01 +0100

    ts function declerations and void

--- sandbox.js
+++ sandbox.js
@@ -1,1 +1,15 @@
-console.log("salsaaaaaaaaaaaaaaaaaaaaaa");
+var greet;
+greet = function () {
+    console.log('hello world');
+};
+var add = function (a, b, c) {
+    if (c === void 0) { c = 10; }
+    console.log(a + b);
+    console.log(c);
+};
+add(5, 10, 20);
+greet();
+var minus = function (a, b) {
+    return a + b;
+};
+var result = minus(10, 7);
